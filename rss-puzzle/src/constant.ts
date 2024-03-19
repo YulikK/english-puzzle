4de9adeb3c7e932b30d9419dc923919e6f3a4d4f
Yuliya Kursevich <54816946+YulikK@users.noreply.github.com>
2024-03-19 09:05:09 +0100

    feat: add specific name for store key (#29)

--- rss-puzzle/src/constant.ts
+++ rss-puzzle/src/constant.ts
@@ -1,5 +1,5 @@
 export const WELCOME_DESCRIPTION = 'Welcome to the world of English and art! Improve your English skills by collecting sentences and uncovering pictures of art. Ready for a challenge? Immerse yourself in the world of words and art with us!'
-const STORE_PREFIX = `RSS_Puzzle`;
+const STORE_PREFIX = `RSS_Puzzle_Yulik`;
 const STORE_VER = `v1`;
 export const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
 export const STORE_USER = `user`;
